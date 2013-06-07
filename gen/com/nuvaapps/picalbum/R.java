27130864ff85f5a2ec935c14985bc7f78e4cfb0d
monur <monur@nuvaapps>
2013-06-07 15:08:51 +0300

    When tapped right, next picture shows up App remembers which picture is shown last time

--- gen/com/nuvaapps/picalbum/R.java
+++ gen/com/nuvaapps/picalbum/R.java
@@ -38,8 +38,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
         public static final int RelativeLayout1=0x7f090002;
-        public static final int action_settings=0x7f090004;
+        public static final int exit=0x7f090005;
         public static final int imageView1=0x7f090001;
+        public static final int save=0x7f090004;
         public static final int textView1=0x7f090003;
     }
     public static final class layout {
--- src/com/nuvaapps/picalbum/PictureActivity.java
+++ src/com/nuvaapps/picalbum/PictureActivity.java
@@ -5,10 +5,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,8 +19,11 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class PictureActivity extends Activity {
-
+	private static final int PICTURE_COUNT = 632;
 	ImageView imageView;
+	SharedPreferences prefs;
+	Bitmap currentImage;
+	int pictureId = 1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +31,32 @@ public class PictureActivity extends Activity {
 		setContentView(R.layout.activity_picture);
 		imageView = (ImageView)findViewById(R.id.imageView1);
 		Toast.makeText(this, "Tap right for next picture, left for previous", Toast.LENGTH_LONG ).show();
+		prefs = getSharedPreferences("PicAlbum", 0);
+		pictureId = prefs.getInt("pictureId", 1);
 		loadPicture();
 		LinearLayout layout = (LinearLayout)findViewById(R.id.LinearLayout1);
 		layout.setOnTouchListener(new View.OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				if(event.getAction() == MotionEvent.ACTION_DOWN)
-					System.out.println(event.getX() +"-" + event.getY());
+				if(event.getAction() == MotionEvent.ACTION_DOWN){
+					if(event.getX() < v.getWidth() /2)
+						pictureId++;
+					else
+						pictureId--;
+					if(pictureId < 1) pictureId = PICTURE_COUNT;
+					if(pictureId > PICTURE_COUNT) pictureId = 1;
+					savePictureId();
+					loadPicture();
+				}
 				return true;
 			}
 		});
+	}
+	
+	private void savePictureId(){
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt("pictureId", pictureId);
+		editor.commit();
 	}
 
 	@Override
@@ -44,11 +66,10 @@ public class PictureActivity extends Activity {
 	}
 	
 	private void loadPicture(){
-		URL url;
 		try {
-			url = new URL("http://subi.milliyet.com.tr/MilliyetMizah/600xMax/2010/11/10/Sarki_soyleyen_at_f48a0.jpg");
-			Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-			imageView.setImageBitmap(bmp);
+			URL url = new URL("http://nuvaapps.zz.mu/pictures/getPicture.php?pictureId=" + pictureId);
+			currentImage = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+			imageView.setImageBitmap(currentImage);
 		} catch (MalformedURLException e) {
 			Toast.makeText(this, "Connection error, check your internet connections",  Toast.LENGTH_LONG).show();
 			e.printStackTrace();
@@ -56,6 +77,23 @@ public class PictureActivity extends Activity {
 			Toast.makeText(this, "Connection error, check your internet connections",  Toast.LENGTH_LONG).show();
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.save:
+			String result = MediaStore.Images.Media.insertImage(getContentResolver(), currentImage, "ai_shinozaki" + pictureId , "Ai Shinozaki " + pictureId);
+			if(result == null)
+				Toast.makeText(this, "Cannot save image",  Toast.LENGTH_LONG).show();
+			else
+				Toast.makeText(this, "Image saved to gallery",  Toast.LENGTH_LONG).show();
+			break;
+		case R.id.exit:
+		default:
+			break;
+		}
+		return true;
 	}
 
 }
